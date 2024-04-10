4271af0be209ef42cfb98d64da9c904cd3eb9645
Youngjin Park <youngjpa@c4r8s1.42seoul.kr>
2024-04-10 22:17:35 +0900

    fix: tdata 디자인 변경

--- frontend/src/components/tournamentData.js
+++ frontend/src/components/tournamentData.js
@@ -14,9 +14,7 @@ export default class extends AbstractComponent {
         return `
         <style>
         .b {
-            /* border: solid 1px black; */
-            max-width: 750px;
-            overflow: auto;
+            max-width: 800px;
             margin: 0 auto;
             margin-bottom: 30px;
         }
@@ -26,12 +24,111 @@ export default class extends AbstractComponent {
             color: brown;
             margin-bottom: 10px;
         }
+        
+        .timestemp {
+            margin-top: 45px;
+            text-align: center;
+            max-width: 200px;
+            font-weight:900;
+        }
+        
+        .tdataBox{
+            border: 2px solid #4D37C6;
+            border-radius: 0.4rem;
+            margin: 5px;
+            border-width: 5px;
+        }
+
+        .game {
+            /* border-bottom: 1px dotted #7c798d; */
+            margin: 5px;
+            height: 30px;
+        }
+        
+        .textAlign {
+            font-size: large;
+            text-align:center;
+            font-weight: bolder;
+        }
+        .winner {
+            color: red;
+        }
+
         </style>
         
         <body>
         
         <div class="container">
-        <h1 class="display-7 title">Pong Tournament Data from Blockchain.</h1>
+
+        <div class="b">
+            <ul class="tdataBox list-group d-flex justify-content-between">
+                <div class="row">
+                    <div class="col-3">
+                    <div class="align-items-center timestemp noto-sans">2024.01.01 13:42</div>
+                    </div>
+                    <div class="col-9">
+                        <div class="game justify-content-between align-items-center my-2 row">
+                                <span class="textAlign col-3 game-rank">1st</span>
+                                <span class="textAlign col game-player">youngjpa</span>
+                                <span class="textAlign col-4 game-score">5 : 3</span>
+                                <span class="textAlign col game-opponent">asdf</span>
+                        </div>
+                        <div class="game justify-content-between align-items-center my-2 row">
+                                <span class="textAlign col-3 game-rank">2st</span>
+                                <span class="textAlign col game-player">youngjpa</span>
+                                <span class="textAlign col-4 game-score">5 : 3</span>
+                                <span class="textAlign col game-opponent">asdf</span>
+                        </div>
+                        <div class="game justify-content-between align-items-center my-2 row">
+                                <span class="textAlign col-3 game-rank">Final</span>
+                                <span class="textAlign col game-player">youngjpa</span>
+                                <span class="textAlign col-4 game-score">5 : 3</span>
+                                <span class="textAlign col game-opponent">asdf</span>
+                        </div>
+                    </div>
+                </div>
+            </ul>
+            <ul class="tdataBox list-group d-flex justify-content-between">
+                <div class="row">
+                    <div class="col-3">
+                    <div class="align-items-center timestemp">2024.01.01 13:42</div>
+                    </div>
+                    <div class="col-9">
+                        <div class="justify-content-between align-items-center my-2">
+                            <div class="row">
+                                <span class="game col-3 game-rank">1st</span>
+                                <span class="game col game-player">youngjpa</span>
+                                <span class="game col-4 game-score">5 : 3</span>
+                                <span class="game col game-opponent">asdf</span>
+                            </div>
+                        </div>
+                        <div class="justify-content-between align-items-center my-2">
+                            <div class="row">
+                                <span class="game col-3 game-rank">2st</span>
+                                <span class="game col game-player">youngjpa</span>
+                                <span class="game col-4 game-score">5 : 3</span>
+                                <span class="game col game-opponent">asdf</span>
+                            </div>
+                        </div>
+                        <div class="justify-content-between align-items-center my-2">
+                            <div class="row">
+                                <span class="game col-3 game-rank">Final</span>
+                                <span class="game col game-player">youngjpa</span>
+                                <span class="game col-4 game-score">5 : 3</span>
+                                <span class="game col game-opponent">asdf</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </ul>
+        </div>
+
+    
+    
+    
+    
+    
+
         <table class="table b">
         <thead>
         <tr>
@@ -49,7 +146,7 @@ export default class extends AbstractComponent {
         
         <div class="container">
         <div class="text-end" id="btn">
-        <button type="button" class="btn btn-primary" id="goBackButton">돌아가기</button>
+        <button type="button" class="btn btn-outline-dark" id="goBackButton">돌아가기</button>
         </div>
         <script src="tournamentData.js"></script>
         </div>
@@ -57,6 +154,27 @@ export default class extends AbstractComponent {
         </body>
 		`;
     }
+
+    populateCardData() {
+        // 카드 데이터를 채우는 함수
+        // 이 부분에서 필요한 HTML 코드를 동적으로 생성하여 반환
+        // 예를 들어:
+        let cardDataHtml = `
+            <div class="card b">
+                <div class="card-header">
+                    <h1 class="display-7 title">Card Data</h1>
+                </div>
+                <!-- 추가적인 HTML 코드 추가 -->
+                <div class="card-body">
+                    <!-- 카드 내용 -->
+                    <p>Some additional information</p>
+                </div>
+            </div>
+        `;
+        return cardDataHtml;
+    }
+
+
     // 토너먼트 데이터를 표에 채우는 함수
     populateTable(data) {
         let tbody = document.getElementById("tournamentData");
@@ -117,14 +235,6 @@ export default class extends AbstractComponent {
                 console.log(error.message);
             }
         })(this.populateTable);
-
-        // fetch("http://localhost:8000/dashboard")
-        //     .then((response) => {
-        //         return response.json();
-        //     })
-        //     .then((data) => {
-        //         this.populateTable(data);
-        //     });
 
         const goBackBtn = document.querySelector("#goBackButton");
         goBackBtn.addEventListener("click", event => {
